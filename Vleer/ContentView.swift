d2b34824b3a457f3b23a0413e56fa1ac4a4811e5
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2024-08-06 13:40:23 +0200

    Initial Commit

---
+++ Vleer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vleer
+//
+//  Created by Nils on 06.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vleer/VleerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VleerApp.swift
+//  Vleer
+//
+//  Created by Nils on 06.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct VleerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
